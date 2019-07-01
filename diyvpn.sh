25f1d5b104becb1c7ef2b57a10ce82dfd4c7fcf6
richard <jiang.j.qian@qq.com>
2019-07-01 19:07:28 +0800

    update a bit

--- diyvpn.sh
+++ diyvpn.sh
@@ -4,7 +4,7 @@ servertundev=tun13
 clienttundev=tun13
 serverip=10.10.0.1
 clientip=10.10.0.100
-port=23
+port=21
 #debug=-d
 debug=
 
@@ -27,16 +27,27 @@ stopserver() {
 }
 
 stopclient() {
-	echo "stop client."
+	echo "Stop client."
 	ip tuntap del mode tun $clienttundev
-	ip route delete $hkserverip via $defaultrouter
-	ip route delete default
-	ip route add default via $defaultrouter
+    echo "Change netplan yaml, the nameserver to 192.168.22.1"
+    read -p "Continue: " unused
+	vi /etc/netplan/50-cloud-init.yaml
+	netplan apply --debug
+	systemd-resolve --flush-caches
+	systemd-resolve --statistics
 }
 
 startclient() {
-	echo "start client."
-	stopclient
+	echo "Start client."
+    if ip link show $clienttundev 2>&1 >/dev/null; then
+		ip tuntap del mode tun $clienttundev
+    fi
+
+    echo "Change netplan yaml, the nameserver to 100.100.2.136"
+    read  -p "Continue: " unused
+	vi /etc/netplan/50-cloud-init.yaml
+	netplan apply --debug
+    read -p "Wait till netplan apply the default router: " unused
 	ip tuntap add mode tun $clienttundev
 	ip addr add $clientip/24 dev $clienttundev
 	ip link set $clienttundev up
@@ -45,6 +56,8 @@ startclient() {
 	ip route add $hkserverip via $defaultrouter
 	ip route delete default via $defaultrouter
 	ip route add default via $serverip
+	systemd-resolve --flush-caches
+    ip route list
 	./simpletun -i $clienttundev -c $hkserverip -p $port $debug
 }
 
