349ae9a4d6eff3942db69caacdd36ee774c79e60
richard <jiang.j.qian@qq.com>
2019-07-01 15:19:04 +0800

    update support diyvpn client start

--- diyvpn.sh
+++ diyvpn.sh
@@ -5,7 +5,12 @@ clienttundev=tun13
 serverip=10.10.0.1
 clientip=10.10.0.100
 port=23
-debug=-d
+#debug=-d
+debug=
+
+#client specific
+defaultrouter=192.168.22.1
+hkserverip=47.75.45.219
 
 startserver() {
 	echo "start server."
@@ -21,12 +26,28 @@ stopserver() {
 	ip tuntap del mode tun $servertundev
 }
 
-startclient() {
-	echo "start client."
-}
-
 stopclient() {
 	echo "stop client."
+	ip tuntap del mode tun $clienttundev
+	ip route delete $hkserverip via $defaultrouter
+	ip route delete default
+	ip route add default via $defaultrouter
+}
+
+startclient() {
+	echo "start client."
+	stopclient
+	ip tuntap add mode tun $clienttundev
+	ip addr add $clientip/24 dev $clienttundev
+	ip link set $clienttundev up
+	ifconfig $clienttundev
+
+	ip route add $hkserverip via $defaultrouter
+	ip route delete default via $defaultrouter
+	ip route add default via $serverip
+	./simpletun -i $clienttundev -c $hkserverip -p $port $debug
+	ip route delete default
+	ip route add default via $defaultrouter
 }
 
 usage() {
